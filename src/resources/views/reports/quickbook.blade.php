cbd8c99f617ef2faafd1e4b3e94d341ddaa74a51
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-11-12 14:05:23 +0530

    fixed the quickbook report css

--- src/resources/views/reports/quickbook.blade.php
+++ src/resources/views/reports/quickbook.blade.php
@@ -3,7 +3,7 @@
 @section('css')
 <style>
     thead, tbody, tfoot, tr, td, th {
-        white-space: pre-wrap;
+        /* white-space: pre-wrap; */
     }
 </style>
 @stop
@@ -60,7 +60,7 @@
                         </div>
                         <div class="p-0 card-body">
                             <div class="mt-4 table-responsive">
-                                <table id="basic-table" class="table mb-0 table-striped" role="grid">
+                                <table id="basic-table" class="table mb-0">
                                     <thead>
                                         <tr>
                                             <th>Event ID</th>
@@ -68,17 +68,16 @@
                                             <th>Client</th>
                                             <th>Writer</th>
                                             <th>Date</th>
-                                            <th>Start <br />Time</th>
-                                            <th>End <br />Time</th>
+                                            <th>Start Time</th>
+                                            <th>End Time</th>
                                             <th>Total Time</th>
-                                            <th>Billing<br /> Rate</th>
+                                            <th>Billing Rate</th>
                                             <th>Total</th>
                                         </tr>
                                     </thead>
                                     @if($data->total() > 0)
-                                        @foreach ($data->items() as $key=>$item)
-                                            @if($key == 0)
-                                                <tbody>
+                                        <tbody>
+                                            @foreach ($data->items() as $key=>$item)
                                                     <tr>
                                                         <td rowspan="{{$item->writers->count()+1}}">
                                                             <div class="d-flex align-items-center">
@@ -115,62 +114,21 @@
                                                             </td>
                                                         </tr>
                                                     @endforeach
-                                                </tbody>
-                                            @endif
-                                        @endforeach
+                                            @endforeach
+                                        </tbody>
+                                    @else
+                                    <tbody>
+                                        <tr>
+                                            <td colspan="10">
+                                                <p class="text-center">No data available</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
                                     @endif
                                 </table>
-                                @if($data->total() > 0)
-                                    @foreach ($data->items() as $key=>$item)
-                                        @if($key != 0)
-                                            <table id="basic-table" class="table mb-0 table-striped" role="grid">
-
-                                                <tbody>
-                                                    <tr>
-                                                        <td rowspan="{{$item->writers->count()+1}}">
-                                                            <div class="d-flex align-items-center">
-                                                                <a href="{{ route('event.view.get', $item->id) }}">EVD{{$item->id}}</a>
-                                                            </div>
-                                                        </td>
-                                                        <td rowspan="{{$item->writers->count()+1}}">
-                                                            <div class="iq-media-group iq-media-group-1">
-                                                                <a href="{{ route('event.view.get', $item->id) }}">{{$item->name}}</a>
-                                                            </div>
-                                                        </td>
-                                                        <td>{{$item->client->name}}</td>
-                                                        <td>&nbsp;</td>
-                                                        <td rowspan="{{$item->writers->count()+1}}">
-                                                            {{$item->start_date->format('M d Y')}}
-                                                        </td>
-                                                        <td rowspan="{{$item->writers->count()+1}}">{{$item->start_time->format('h:i a')}}</td>
-                                                        <td rowspan="{{$item->writers->count()+1}}">{{$item->end_time->format('h:i a')}}</td>
-                                                        <td rowspan="{{$item->writers->count()+1}}"> 1 Hour</td>
-                                                        <td> {{$item->invoice_rate}} $ / hr </td>
-                                                        <td>
-                                                            {{$item->invoice_rate}} $
-                                                        </td>
-                                                    </tr>
-                                                    @foreach($item->writers as $k=>$v)
-                                                        <tr>
-                                                            <td> </td>
-                                                            <td>{{$v->writer->name}}</td>
-
-
-                                                            <td> {{$v->billing_rate}} $ / hr </td>
-                                                            <td>
-                                                                {{$v->billing_rate}} $
-                                                            </td>
-                                                        </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                        @endif
-                                    @endforeach
-                                @else
-                                    <p class="text-center">No data available</p>
-                                @endif
                             </div>
-                            <div style="padding: 20px; text-align: right; "><a href="#" class="btn btn-primary">
+                            <div style="padding: 20px; text-align: right; ">
+                                <a href="#" class="btn btn-primary">
                                     <svg class="icon-32" width="32" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path opacity="0.4"
@@ -179,7 +137,9 @@
                                         <path
                                             d="M7.72033 12.8555L11.4683 16.6205C11.7503 16.9035 12.2493 16.9035 12.5323 16.6205L16.2803 12.8555C16.5723 12.5615 16.5713 12.0865 16.2773 11.7945C15.9833 11.5025 15.5093 11.5025 15.2163 11.7965L12.7493 14.2735V7.91846C12.7493 7.50346 12.4133 7.16846 11.9993 7.16846C11.5853 7.16846 11.2493 7.50346 11.2493 7.91846V14.2735L8.78333 11.7965C8.63633 11.6495 8.44433 11.5765 8.25133 11.5765C8.06033 11.5765 7.86833 11.6495 7.72233 11.7945C7.42933 12.0865 7.42833 12.5615 7.72033 12.8555Z"
                                             fill="currentColor"></path>
-                                    </svg> Export</a></div>
+                                    </svg> Export
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
